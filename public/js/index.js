face5e77941a34507b3a8fb40c0057be0a6334ee
Ritik Mody <ritikashishmody@gmail.com>
2020-11-14 01:16:18 +0530

    Dark mode improved

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,15 @@
 var toggle = false;
 $('.dark').click(()=>{
+  document.body.classList.toggle('dark-mode');
   if(!toggle)
   {
     toggle = true;
-    $(".css").html("<link rel='stylesheet' href='/css/dark.css'>");
     $(".dark").html("<i class='fas fa-moon fa-2x'></i>");
   }
   else
   {
     toggle = false;
-    $(".css").html("<link rel='stylesheet' href='/css/styles.css'>");
+    // $(".css").html("<link rel='stylesheet' href='/css/styles.css'>");
     $(".dark").html("<i class='fas fa-sun fa-2x'></i>");
   }
 });
